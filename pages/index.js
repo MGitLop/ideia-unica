a9992800b8a8de7b68cfe8e302e9093115c37643
MGitLop <mcursoslapdell@gmail.com>
2021-12-17 18:43:41 -0300

    :sparkles: "Introduce Link from nextJS"
    #

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,11 @@
-import { useState } from 'react';
-
+import Link from 'next/link'
 function Home(){
   return(
     <div>
       <h1>Pagina teste</h1>
-      <p>Teste de deploy no VERCEL</p>
-      <a href="/sobre">Acessar página Sobre</a>
-      <Contador />  
-    </div>
-
-  )
-}
-
-function Contador(){
-  const [contador,setContador] = useState(1);
-
-  function adicionarContador(){
-    setContador(contador + 1);
-  }
-
-  return (
-    <div>
-      <div>{contador}</div>
-      <button onClick={adicionarContador}>Adicionar</button>
+      <Link href="/sobre">
+        <a>Acessar página Sobre</a>
+      </Link>
     </div>
   )
 }
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,11 +1,14 @@
+import Link from 'next/link';
+
 function Sobre(){
   return(
     <div>
       <h1>Sobre</h1>
       <p>Nova página sobre inclusa!</p>
-      <a href="/">Acessar página Home</a>
+      <Link href="/"> 
+      <a>Acessar página Home</a>
+      </Link>
     </div>
-
   )
 }
 
